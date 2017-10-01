38c2cb72dcc000ae57a9fff29b0486cf86c19161
Raj Bennin <rajisteb@gmail.com>
2017-10-01 11:59:56 -0400

    httparty response confirmed

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -3,7 +3,8 @@
 class HeadlinesController < ApplicationController 
 
     def home
-
+        response = HTTParty.get('https://api.iextrading.com/1.0/stock/aapl/batch?types=quote,news,chart&range=1m&last=1')
+        @res = JSON.parse response.to_s
     end
 
 
