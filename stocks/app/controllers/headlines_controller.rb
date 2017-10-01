ae51a2c6c8a66cfdf219d7d36eb45dfb35390f1e
Raj Bennin <rajisteb@gmail.com>
2017-10-01 19:17:02 -0400

    get api call text and style

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -3,8 +3,22 @@
 class HeadlinesController < ApplicationController 
 
     def home
+        key = Rails.application.secrets.bing_api_search_key || ENV['BING_API_SEARCH_KEY']
         response = HTTParty.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,googl,nflx&types=quote')
-        @board = JSON.parse response.to_s, symbolize_names: true
+        news = HTTParty.get('https://api.iextrading.com/1.0/stock/market/news/last/7')
+
+        def jsonParse(result)
+            return JSON.parse result.to_s, symbolize_names: true
+        end
+        
+        @board = jsonParse(response)
+        @story = jsonParse(news)
+        
+        def images(res)
+            pictures = HTTParty.get("https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=#{res}", :headers=> {'Ocp-Apim-Subscription-Key': key})
+            return jsonParse(pictures)
+        end
+
     end
 
 
