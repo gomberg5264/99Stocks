0727cae33ccb44961a5147d28412d17a2ec40a61
Raj Bennin <rajisteb@gmail.com>
2017-10-01 16:14:17 -0400

    complete style and api render for stock board

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -3,8 +3,8 @@
 class HeadlinesController < ApplicationController 
 
     def home
-        response = HTTParty.get('https://api.iextrading.com/1.0/stock/aapl/batch?types=quote,news,chart&range=1m&last=1')
-        @res = JSON.parse response.to_s
+        response = HTTParty.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,googl,nflx&types=quote')
+        @board = JSON.parse response.to_s, symbolize_names: true
     end
 
 
