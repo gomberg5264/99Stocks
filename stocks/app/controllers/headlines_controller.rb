d8930daeb11dbe8ed95995c5fc0700b7eb497da8
Raj Bennin <rajisteb@gmail.com>
2017-10-02 23:48:55 -0400

    scrap news template create dashboard

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -8,7 +8,7 @@ class HeadlinesController < ApplicationController
 
     def home
         response = HTTParty.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,googl,nflx&types=quote')
-        news = HTTParty.get('https://api.iextrading.com/1.0/stock/market/news/last/7')
+        news = HTTParty.get('https://api.iextrading.com/1.0/stock/market/news/last/35')
         
         def jsonParse(result)
             return JSON.parse result.to_s, symbolize_names: true
@@ -18,15 +18,7 @@ class HeadlinesController < ApplicationController
         @story = jsonParse(news)
         
         
-        def images(res)
-            encoded = URI.escape(res.split(' ')[0...6].join(" "))
-            @subscription = '8a3446dcf7c64ea79f747bb39b88d540'
-            #picture from bing image search for headline
-            pictures = HTTParty.get("https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=#{encoded}", :headers=> {"Ocp-Apim-Subscription-Key": @subscription})
-            parsed = jsonParse(pictures)
-            # return imgUrl = URI.unescape(encoded).to_sym
-
-        end
+    
 
     end
 
