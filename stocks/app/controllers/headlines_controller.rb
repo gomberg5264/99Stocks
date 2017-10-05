8ad554eb3e0bbb58b500534839e27e8c321d6aea
Raj Bennin <rajisteb@gmail.com>
2017-10-05 01:47:19 -0400

    dashboard links working

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -7,6 +7,10 @@ class HeadlinesController < ApplicationController
     helper_method :images
 
     def home
+
+    end
+
+    def dashboard
         newsKey = Rails.application.secrets.news_api_key || ENV["NEWS_API_KEY"]
         response = HTTParty.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,googl,nflx&types=quote')
         news = HTTParty.get('https://api.iextrading.com/1.0/stock/market/news/last/35')
@@ -39,7 +43,8 @@ class HeadlinesController < ApplicationController
                 else
                     news_arr[count] = {
                         :title => (@news[:articles][i][:title]),
-                        :urlToImage => (@news[:articles][i][:urlToImage])
+                        :urlToImage => (@news[:articles][i][:urlToImage]),
+                        :url => (@news[:articles][i][:url])
                     }
                     count += 1
                 end
@@ -70,8 +75,51 @@ class HeadlinesController < ApplicationController
         
         @shortTweets = stockTweets
         @topNews = getNews
+
+    end
+
+    def single
+        params[:id]
+        stockTwits = HTTParty.get('https://api.stocktwits.com/api/2/streams/trending.json')
+        quote = HTTParty.get("https://api.iextrading.com/1.0/stock/#{params[:id]}/quote?displayPercent=true")
+        company = HTTParty.get("https://api.iextrading.com/1.0/stock/#{params[:id]}/company")
+        keystats = HTTParty.get("https://api.iextrading.com/1.0/stock/#{params[:id]}/company")
+        financials = HTTParty.get("https://api.iextrading.com/1.0/stock/#{params[:id]}/company")
+        logo = HTTParty.get("https://api.iextrading.com/1.0/stock/#{params[:id]}/logo")
+
+        def jsonParse(result)
+            return JSON.parse result.to_s, symbolize_names: true
+        end
+
+        def stockTweets 
+            tweet_arr = Array.new
+            count = 0
+            
+            (0..29).each do |i|
+                if count > 2
+                    break
+                elsif @stockTwits[:messages][i][:body].length < 79 && @stockTwits[:messages][i][:body].length > 25 && 
+                    @stockTwits[:messages][i][:user][:avatar_url] != "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg"
+                    tweet_arr[count] = {
+                        :name => (@stockTwits[:messages][i][:user][:name]),
+                        :image => (@stockTwits[:messages][i][:user][:avatar_url]),
+                        :body => (@stockTwits[:messages][i][:body])
+                    }
+                    count += 1
+                end
+            end
+            return tweet_arr
+        end
         
-    
+
+        @stockTwits = jsonParse(stockTwits)
+        @quote = jsonParse(quote)
+        @company = jsonParse(company)
+        @keystats = jsonParse(keystats)
+        @financials = jsonParse(financials)
+        @logo = jsonParse(logo)
+        @shortTweets = stockTweets
+        
 
     end
 
