56b6114023870ab8200ea75fd884bf79e2f8d50f
Raj Bennin <rajisteb@gmail.com>
2017-10-04 21:46:37 -0400

    added stocktwits render from api and styled

--- stocks/app/controllers/headlines_controller.rb
+++ stocks/app/controllers/headlines_controller.rb
@@ -11,7 +11,8 @@ class HeadlinesController < ApplicationController
         news = HTTParty.get('https://api.iextrading.com/1.0/stock/market/news/last/35')
         topGainers = HTTParty.get('https://api.iextrading.com/1.0//stock/market/list/gainers')
         topLosers = HTTParty.get('https://api.iextrading.com/1.0//stock/market/list/losers')
-        
+        stockTwits = HTTParty.get('https://api.stocktwits.com/api/2/streams/trending.json')
+
         def jsonParse(result)
             return JSON.parse result.to_s, symbolize_names: true
         end
@@ -20,7 +21,29 @@ class HeadlinesController < ApplicationController
         @story = jsonParse(news)
         @gainers = jsonParse(topGainers)
         @losers = jsonParse(topLosers)
-        
+        @stockTwits = jsonParse(stockTwits)
+
+        def stockTweets 
+            tweet_arr = Array.new
+            count = 0
+            
+            (0..29).each do |i|
+                if count > 2
+                    break
+                elsif @stockTwits[:messages][i][:body].length < 79 && @stockTwits[:messages][i][:body].length > 25 && 
+                    @stockTwits[:messages][i][:user][:avatar_url] != "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg"
+                    tweet_arr[count] = {
+                        :name => (@stockTwits[:messages][i][:user][:name]),
+                        :image => (@stockTwits[:messages][i][:user][:avatar_url]),
+                        :body => (@stockTwits[:messages][i][:body])
+                    }
+                    count += 1
+                end
+            end
+            return tweet_arr
+        end
+
+        @shortTweets = stockTweets
         
     
 
